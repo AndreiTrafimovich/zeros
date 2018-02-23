9a5e0091f792cdd364371e31aa3d7444c880ab65
atr <atr@ciklum.com>
2018-02-23 23:40:23 +0300

    Find zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    var count = 0;
+    var i = 5;
+
+    while (i<=number){
+        count += Math.floor(number/i);
+        i = i*5
+    }
+
+    return count;
 }
